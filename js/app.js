6e80d0e14c1e7834f3e1c8bfa28e83e2a665f6c5
sides <sides@sides.tv>
2016-01-31 04:56:32 +0000

    Don't remove the bg-video-static class until after the video has loaded

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 $(function() {
 	if (!isMobile.any) {
 		$('.bg-video-container[data-bg-video]').each(function() {
-			var t = $(this),
-				path = '/assets/img/' + t.data('bg-video');
-			t.prepend('<video class="bg-video" loop muted autoplay poster="' + path + '.jpg"><source src="' + path + '.webm" type="video/webm"></source><source src="' + path + '.mp4" type="video/mp4"></source><source src="' + path + '.ogv" type="video/ogg"></source></video>');
-			t.removeClass('bg-video-static');
+			var $t = $(this),
+				path = '/assets/img/' + $t.data('bg-video');
+			var $vid = $('<video class="bg-video" loop muted autoplay poster="' + path + '.jpg"><source src="' + path + '.webm" type="video/webm"></source><source src="' + path + '.mp4" type="video/mp4"></source><source src="' + path + '.ogv" type="video/ogg"></source></video>');
+			$vid.on('load', function() {
+				$t.removeClass('bg-video-static');
+			});
+			$t.prepend($vid);
 		});
 	}
-})
+});
