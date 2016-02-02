404e48f8f5c4945a6f1ff771f3d9d4f2e2e373dc
sides <sides@sides.tv>
2016-02-02 15:25:34 +0000

    Don't use onload to remove bg-video-static, do it as soon as/if bg-video starts playing

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(function() {
 			var $t = $(this),
 				path = '/assets/img/' + $t.data('bg-video');
 			var $vid = $('<video class="bg-video" loop muted autoplay poster="' + path + '.jpg"><source src="' + path + '.webm" type="video/webm"></source><source src="' + path + '.mp4" type="video/mp4"></source><source src="' + path + '.ogv" type="video/ogg"></source></video>');
-			$vid.on('load', function() {
+			$vid.on('playing', function() {
 				$t.removeClass('bg-video-static');
 			});
 			$t.prepend($vid);
