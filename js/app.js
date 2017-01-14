13445ee0f74bee938df6f61bc5190eb95f5e7933
sides <sides@sides.tv>
2017-01-14 19:33:39 +0000

    Update contact page

--- js/app.js
+++ js/app.js
@@ -10,4 +10,15 @@ $(function() {
 			$t.prepend($vid);
 		});
 	}
+
+  $('[data-mail]').each(function() {
+    var $this = $(this),
+      mail = $this.data('mail') || $this.html();
+    mail = mail.replace(' ', '@').replace(' ', '.');
+    if ($this.is('a')) {
+      $this.attr('href', 'mailto:' + mail);
+    } else {
+      $this.html(mail);
+    }
+  });
 });
