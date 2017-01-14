d8c891dbe9ac64830fa413e991f122093ba9f857
sides <sides@sides.tv>
2017-01-14 21:17:53 +0000

    Don't need ogv

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(function() {
 		$('.jumbotron-video-container[data-jumbotron-video]').each(function() {
 			var $t = $(this),
 				path = '/assets/img/' + $t.data('jumbotron-video');
-			var $vid = $('<video class="jumbotron-video" loop muted autoplay poster="' + path + '.jpg"><source src="' + path + '.webm" type="video/webm"></source><source src="' + path + '.mp4" type="video/mp4"></source><source src="' + path + '.ogv" type="video/ogg"></source></video>');
+			var $vid = $('<video class="jumbotron-video" loop muted autoplay poster="' + path + '.jpg"><source src="' + path + '.webm" type="video/webm"></source><source src="' + path + '.mp4" type="video/mp4"></source></video>');
 			$vid.on('playing', function() {
 				$t.removeClass('jumbotron-video-static');
 			});
