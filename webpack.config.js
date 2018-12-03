8997afb669ded94dc2f7d075e62ed40c8c76b625
sides <sides@sides.tv>
2018-12-03 18:06:09 +0000

    Add app.route based on index instead of direct links

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ const config = {
       },
       {
         test: /\.pug$/,
-        use: ['pug-loader']
+        use: 'pug-loader'
       },
       {
         test: /\.scss$/,
@@ -34,14 +34,7 @@ const config = {
       },
       {
         test: /\.(png|jpg|gif|webm|mp4)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'assets/[hash].[ext]'
-            }
-          }
-        ]
+        use: [{ loader: 'file-loader', options: { name: 'assets/[hash].[ext]' } }]
       }
     ]
   },
@@ -53,24 +46,24 @@ const config = {
   plugins: Object.keys(index).map(page => new HtmlWebpackPlugin({
     filename: index[page] + '.html',
     template: './html/' + page + '.pug',
-    inject: true
+    templateParameters: {
+      index: index,
+      currentRoute: page
+    }
   }))
 };
 
 module.exports = (env, argv) => {
   const production = argv.mode === 'production';
 
-  // dumb
   config.plugins.push(new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify(argv.mode)
   }));
 
-  // also dumb
   if (!production) {
     config.devServer.hot = true;
     config.plugins.push(new webpack.HotModuleReplacementPlugin());
   } else {
-    // terrible
     config.module.rules[2].use[0].loader = MiniCssExtractPlugin.loader;
     config.plugins.push(new MiniCssExtractPlugin({
       filename: 'assets/app.[contenthash].css'
